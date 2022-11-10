9d35a9a76a1759aeef4356e2490166c4f1b837b5
Shubhankar Valimbe <59073366+shubhankarval@users.noreply.github.com>
2022-11-09 22:57:08 -0500

    Update info.py
    added license

--- src/info.py
+++ src/info.py
@@ -1,3 +1,12 @@
+'''
+Copyright (c) 2022, Shubhankar Valimbe
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree. 
+
+'''
+
 usd = "USD - United States Dollar"
 inr = "INR - Indian Rupee"
 
