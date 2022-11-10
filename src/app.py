e1b8529b6f00795336284021d3a44184c983a537
Shubhankar Valimbe <59073366+shubhankarval@users.noreply.github.com>
2022-11-09 22:55:43 -0500

    Update app.py
    added license

--- src/app.py
+++ src/app.py
@@ -1,3 +1,12 @@
+'''
+Copyright (c) 2022, Shubhankar Valimbe
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree. 
+
+'''
+
 import requests
 
 # returns converted currency value as a string
